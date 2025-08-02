3288a20f11c236de9076c7004815317b1774526a
shaltoni <user@shaltonis-MacBook-Pro.local>
2025-08-02 14:02:23 +0300

    day 79  - create the first view (signup) work with style CSS & google fonts

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require('path')
 const authRouter = require('./routes/auth.routes')
 const app = express();
 app.set('view engine' , 'ejs')
-app.set('views', path.join(__dirname, ' viwes'))
-
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static('public'))
 app.use(authRouter);
 
 app.listen(3000)
